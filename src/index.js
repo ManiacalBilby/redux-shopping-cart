54cba00f5a8c9796283fc46a84d4d210b66b74a5
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-01-20 10:24:06 -0500

    completed code along

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import './stylesheets/index.css';
+import App from './components/App';
+import Store from './Store'
+import registerServiceWorker from './registerServiceWorker';
+
+const StoreInstance = Store()
+
+ReactDOM.render(
+    <Provider store={StoreInstance}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
+registerServiceWorker();
